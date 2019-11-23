fc87fc3d7f4b130e643db535eeb18ed6c4d02dd2
Andres <Andres@DESKTOP-CMCMJMS>
2019-11-23 13:37:31 -0500

    NumberPrimes

---
+++ Prime.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using Prime.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<PrimeViewModel>();
+        }
+    }
+}
---
+++ Prime.Core/ViewModels/PrimeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using Prime.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Prime.Core.ViewModels
+{
+    public class PrimeViewModel : MvxViewModel
+    {
+       
+        private MvxCommand _calculateCommand;
+        private readonly IPrimeService _primeService;
+        private int _n;
+        private string _result;
+
+        public PrimeViewModel(IPrimeService primeService)
+        {
+            _primeService = primeService;
+        }
+        public int N
+        {
+            get => _n;
+            set => SetProperty(ref _n, value);
+        }
+
+        public async override Task Initialize()
+        {
+            await base.Initialize();
+            N = 20;
+            Calculate();
+        }
+        public ICommand CalculateCommand
+        {
+            get
+            {
+                _calculateCommand = _calculateCommand ?? new MvxCommand(Calculate);
+                return _calculateCommand;
+            }
+        }
+        public string Result
+        {
+
+            get => _result;
+            set => SetProperty(ref _result, value);
+            
+        }
+        private void Calculate()
+        {
+            Result = _primeService.GetPrime(N);
+        }
+    }
+}
+
