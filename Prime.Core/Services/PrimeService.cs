076666b3ac542ddf37eda39f3393f7f94a201921
Thanathos3096 <36140844+Thanathos3096@users.noreply.github.com>
2019-11-25 18:23:47 -0500

    Update PrimeService.cs

--- Prime.Core/Services/PrimeService.cs
+++ Prime.Core/Services/PrimeService.cs
@@ -1,1 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.Core.Services
+{
+    public class PrimeService : IPrimeService
+    {
+        public string GetPrime(int N)
+        {
+            int conPri = 0, i = 1;
+            string primo = "";
+            
+            while (conPri < N)
+            {
+                if(IsPrime(i))
+                {
+                    primo += String.Format("{0:N0}\t",i);
+                    conPri++;
+                }
+                i++;
+            }
+            return primo;               
+        }
+        private bool IsPrime(long M)
+        {
+            if (M <= 1) return false;
+            for (int j = 2; j <= M / 2; j++)
+            {
+                if (M % j == 0) return false;
+
+            }
+            return true;
+        }
+    }
+}
 
